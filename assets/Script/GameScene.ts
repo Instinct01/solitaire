2ca2708c5cb3eaabb18f1877d5890f2c7f5961d6
Beholder <23341117977@qq.com>
2022-05-23 00:14:16 +0800

    增加发牌区

--- assets/Script/GameScene.ts
+++ assets/Script/GameScene.ts
@@ -4,15 +4,12 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class GameScene extends cc.Component {
-
-    @property(cc.Label)
-    label: cc.Label = null;
-
     @property(cc.Prefab)
     pokerPrefab: cc.Prefab = null;
 
     @property(cc.Node)
     pokerContainer: cc.Node = null;
+
 
     private gameController: GameController = null;
 
@@ -21,7 +18,6 @@ export default class GameScene extends cc.Component {
     }
 
     start () {
-        this.label.string = "这是游戏场景";
         this.gameController = new GameController();
         this.gameController.init(this.pokerContainer, this.pokerPrefab);
         this.gameController.Start();
--- assets/Script/UIPoker.ts
+++ assets/Script/UIPoker.ts
@@ -1,6 +1,7 @@
+import { ESuit } from "./Config";
 import Poker from "./Poker";
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 const POINT_MAP = {
     "1": "A",
@@ -30,6 +31,7 @@ export default class SlashScene extends cc.Component {
     @property(cc.Label)
     pointLaber: cc.Label = null;
 
+    // resource
     @property(cc.SpriteFrame)
     bigSuit: cc.SpriteFrame[] = [];
 
@@ -43,17 +45,19 @@ export default class SlashScene extends cc.Component {
     texFrontBG: cc.SpriteFrame = null;
 
     @property(cc.SpriteFrame)
-    texbackBG: cc.SpriteFrame = null;
+    texBackBG: cc.SpriteFrame = null;
 
-    redTextColor: cc.Color = cc.Color.RED; // 初始化红色的数字颜色
-    blackTextColor: cc.Color = cc.Color.BLACK; // 初始化黑色的数字颜色
+    private redTextColor: cc.Color = cc.color(183, 24, 40); // 初始化红色的数字颜色
+    private blackTextColor: cc.Color = cc.Color.BLACK; // 初始化黑色的数字颜色
 
-    public init (poker: Poker) { // TODO
-        this.pointLaber.string = `${POINT_MAP[poker.point]}`; // 点数
-        if (poker.point >= 11) {
-            this.bigSuitSprite.spriteFrame = this.bigSuit[poker.point - 11]; // 大花色
+    // 一副poker的样式，点数，大花色，小花色，颜色
+    public init(poker: Poker) {
+        this.pointLaber.string = `${POINT_MAP[poker.point]}`; // 通过映射表去获取相应的点数
+        this.pointLaber.node.color = (poker.suit == ESuit.HeiTao || poker.suit == ESuit.MeiHua) ? this.blackTextColor : this.redTextColor; // 颜色
+        if (poker.point >= 11) { // 大花色
+            this.bigSuitSprite.spriteFrame = this.texFaces[poker.point - 11];
         } else {
-            this.bigSuitSprite.spriteFrame = this.bigSuit[poker.suit]; // 大花色
+            this.bigSuitSprite.spriteFrame = this.bigSuit[poker.suit];
         }
         this.smallSuitSprite.spriteFrame = this.smallSuit[poker.suit]; // 小花色
     }
